a05b170666f6b99a0908f7fdf179f9d8703b55c1
Swung0x48 <swung0x48@outlook.com>
2021-07-14 23:23:20 +0800

    Add new thingy to CMakeLists and main header

--- includes/AMMOProtocol.hpp
+++ includes/AMMOProtocol.hpp
@@ -4,5 +4,7 @@
 #include "common/common.hpp"
 #include "common/packet.hpp"
 #include "entity/entity_all.hpp"
+#include "network/network_all.hpp"
+#include "structure/structure_all.hpp"
 
 #endif //AMMOPROTOCOL_BMMOPROTOCOL_HPP
