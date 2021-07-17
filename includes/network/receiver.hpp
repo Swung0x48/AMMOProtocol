51a209c194bb6b98397699e4165907b1640b9ed5
Swung0x48 <swung0x48@outlook.com>
2021-07-17 12:55:41 +0800

    receiver WIP (alive but dunno correctness)

--- includes/network/receiver.hpp
+++ includes/network/receiver.hpp
@@ -27,6 +27,9 @@ namespace ammo::network {
 
         virtual ~receiver() = default;
 
+        virtual void start_receiving() {
+            read_message();
+        }
     private:
         std::string endpoint_to_string(const asio::ip::udp::endpoint& endpoint) {
             return endpoint.address().to_string() + ':' + std::to_string(endpoint.port());
@@ -36,21 +39,29 @@ namespace ammo::network {
             incoming_messages_.push_back({ current_remote_endpoint_, current_incoming_message_ });
         }
 
+        void parse_message_from_buffer() {
+//            std::memcpy(current_incoming_message_.header, buffer_, sizeof(current_incoming_message_.header));
+            std::memcpy(buffer_.data(), &current_incoming_message_.header, sizeof(current_incoming_message_.header));
+            current_incoming_message_.body.resize(current_incoming_message_.header.message_size);
+            std::memcpy(buffer_.data() + sizeof(current_incoming_message_.header), current_incoming_message_.body.data(), current_incoming_message_.body.size());
+        }
+
         // async
         void read_message() {
             socket_.async_receive_from(
                     asio::buffer(buffer_), current_remote_endpoint_,
                     [this](const asio::error_code &error, size_t bytes_transferred) {
                         if (!error) {
-                            current_incoming_message_(buffer_, bytes_transferred);
+//                            current_incoming_message_(buffer_, bytes_transferred);
+                            parse_message_from_buffer();
                             if (!current_incoming_message_.unpack_and_verify()) {
                                 std::cout << "[WARN] From " << endpoint_to_string(current_remote_endpoint_) << "\n";
                                 std::cout << "[WARN] Packet failed on unpack_and_verify.\n";
-                                std::cout << "[WARN] Packet probably corrupted." << std::endl;
-                                return;
+                                std::cout << "[WARN] Packet probably corrupted." << '\n';
+                                std::cout << "[WARN] Bytes transferred: " << bytes_transferred << std::endl;
+                            } else {
+                                add_to_incoming_message_queue();
                             }
-
-                            add_to_incoming_message_queue();
                         }
                         read_message();
                     });
