1db6bebcad2297306a94a97d3f43706640418a34
Swung0x48 <swung0x48@outlook.com>
2021-09-09 11:35:36 +0800

    fix: an empty test to make cmake happy

---
+++ tests/test_reliable_channel.cpp
@@ -0,0 +1,23 @@
+#ifndef AMMOPROTOCOL_RELIABLE_CHANNEL_TEST_HPP
+#define AMMOPROTOCOL_RELIABLE_CHANNEL_TEST_HPP
+
+#include <gtest/gtest.h>
+
+//template<typename T>
+//class test_reliable_channel: ammo::network::reliable_channel<T> {
+//    void on_acked(uint32_t sequence) override {
+//
+//    }
+//
+//    void on_message(ammo::common::owned_message<T>& msg) override {
+//
+//    }
+//};
+
+TEST(ReliableChannel, BasicAssertions) {
+//    test_reliable_channel<uint32_t> channel;
+
+}
+
+
+#endif //AMMOPROTOCOL_RELIABLE_CHANNEL_TEST_HPP
